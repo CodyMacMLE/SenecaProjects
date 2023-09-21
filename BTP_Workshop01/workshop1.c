25a6df6b621bccc42eab3993a97858d225fdc4cc
CodyMacMLE <codymacdonald.dsml@gmail.com>
2023-09-21 15:41:02 -0400

    Update

---
+++ BTP_Workshop01/workshop1.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Workshop 01 - Cody MacDonald (BTP100NAA)
+
+int main() {
+    int variable1;
+    int variable2;
+
+    // Accepting variable 1
+    printf("Enter an integer for variable 1: \n");
+    scanf("%d", &variable1);
+
+    // Clearing buffer
+    while ((getchar()) != '\n');
+
+    // Accepting variable 2
+    printf("Enter an integer for variable 2: \n");
+    scanf("%d", &variable2);
+
+    // Clearing buffer
+        // Since the program will end after accepting variable 2 is this necessary?
+        // or does it stay in to maintain proper practice?
+    while ((getchar()) != '\n');
+
+    // Addition Results
+    printf("Addition: %d + %d = %d\n", variable1, variable2, variable1 + variable2);
+    // Subtraction Results
+    printf("Subtraction: %d - %d = %d\n", variable1, variable2, variable1 - variable2);
+    // Multiplication Results
+    printf("Multiplication: %d * %d = %d\n", variable1, variable2, variable1 * variable2);
+    // Division Results
+    printf("Division: %d / %d = %d\n", variable1, variable2, variable1 / variable2);
+    // Logic Comparison Results
+    printf("Comparison: %d > %d = %d\n", variable1, variable2, variable1 > variable2);
+
+    return 0;
+}
---
+++ BTP_Workshop02/workshop2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main() {
+    int heightFeet, heightInches, heightMeters, heightCentimeters;
+    float totalHeightCentimeters;
+
+    // Accepting User Input
+    printf("Enter your height in the following format (feet inches):\n");
+    scanf("%d %d", &heightFeet, &heightInches);
+    printf("You inputed %d feet %d inches.\n", heightFeet, heightInches);
+
+    //Clearing Buffer
+    while ((getchar())!='\n');
+
+    //Converting imperial inputs to metric centimeters
+    totalHeightCentimeters = (heightFeet*30.48)+(heightInches*2.54);
+    //Seperating metric values
+    heightMeters = (int)totalHeightCentimeters/100;
+    heightCentimeters = totalHeightCentimeters-(heightMeters*100);
+
+    //Print Results
+    printf("You are %dm %dcm tall!", heightMeters, heightCentimeters);
+
+    return 0;
+}
